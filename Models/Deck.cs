47c52a57429106a9364725be9189c3ea0ba5141f
Paweł Trela <paweltrelapro@gmail.com>
2025-05-31 22:14:35 +0100

    validations

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Flashcards.Models;
@@ -6,8 +7,11 @@ public class Deck
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Nazwa wymagana")]
     public string Name { get; set; } = "";
 
+    [Required(ErrorMessage = "Limit pytań wymagany")]
+    [Range(1, int.MaxValue, ErrorMessage = "Musi być większe niż 0.")]
     public int? SessionLimit { get; set; }
 
     public string UserId { get; set; }
